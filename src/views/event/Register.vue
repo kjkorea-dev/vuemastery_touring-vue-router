75b0b8420f5ec1f9cdc6fafd407d26dec2e46bd1
kj <agassi77@gmail.com>
2021-08-18 20:11:29 +0900

    Nested Routes

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,41 +1,15 @@
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <div id="nav">
-      <router-link :to="{ name: 'EventDetails', params: { id } }"
-        >Details</router-link
-      >
-      |
-      <router-link :to="{ name: 'EventRegister', params: { id } }"
-        >Register</router-link
-      >
-      |
-      <router-link :to="{ name: 'EventEdit', params: { id } }"
-        >Edit</router-link
-      >
-    </div>
-    <p>Register in here</p>
-  </div>
+  <p>Register the event here</p>
 </template>
 
 <script>
-import EventService from '@/services/EventService.js'
 export default {
   name: 'EventRegister',
-  props: ['id'],
-  data() {
-    return {
-      event: null
+  props: {
+    event: {
+      type: Object,
+      required: true
     }
-  },
-  created() {
-    EventService.getEvent(this.id)
-      .then(response => {
-        this.event = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
   }
 }
 </script>
