87cf0c1d69e7f3248051f335d0f2418a936022ef
kj <agassi77@gmail.com>
2021-08-19 16:40:03 +0900

    In-Component Route Guard : beforeRouteLeave example

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,10 +1,25 @@
 <template>
-  <p>Edit the event here</p>
+  <p @click="unsavedChanges = !unsavedChanges">
+    Edit the event here {{ unsavedChanges ? '(Editing...)' : '' }}
+  </p>
 </template>
 
 <script>
 export default {
   name: 'EventEdit',
+  data() {
+    return {
+      unsavedChanges: false
+    }
+  },
+  beforeRouteLeave() {
+    if (this.unsavedChanges) {
+      const answer = window.confirm(
+        'Do you really want to leave? You have unsaved chnages!'
+      )
+      if (!answer) return false
+    }
+  },
   props: {
     event: {
       type: Object,
