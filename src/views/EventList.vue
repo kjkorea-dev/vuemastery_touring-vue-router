68dfa5ff95c5f5008edb4454a5a8371577cc354c
kj <agassi77@gmail.com>
2021-08-20 09:23:49 +0900

    In-Component Route Guard : progress bar

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -24,7 +24,7 @@
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
-import { watchEffect } from 'vue'
+import NProgress from 'nprogress'
 
 export default {
   name: 'EventList',
@@ -43,20 +43,56 @@ export default {
       totalEvents: 0
     }
   },
-  created() {
-    watchEffect(() => {
-      this.events = null
-      EventService.getEvents(2, this.page)
-        .then(response => {
-          this.events = response.data
-          this.totalEvents = response.headers['x-total-count']
+  /**
+   * Not work with error
+   * [Vue warn]: The "next" callback was never called inside of "beforeRouteEnter"
+   * */
+  /* beforeRouteEnter(routeTo, routeFrom, next) {
+    NProgress.start()
+    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then(response => {
+        next(comp => {
+          comp.events = response.data
+          comp.totalEvents = response.headers['x-total-count']
         })
-        .catch(() => {
-          this.$router.push({
-            name: 'NetworkError'
-          })
-        })
-    })
+      })
+      .catch(() => {
+        next({ name: 'NetworkError' })
+      })
+      .finally(() => {
+        NProgress.done()
+      })
+  }, */
+  async beforeRouteEnter(routeTo, routeFrom, next) {
+    try {
+      NProgress.start()
+      const response = await EventService.getEvents(
+        2,
+        parseInt(routeTo.query.page) || 1
+      )
+      next(async comp => {
+        comp.events = await response.data
+        comp.totalEvents = await response.headers['x-total-count']
+      })
+    } catch (e) {
+      next({ name: 'NetworkError' })
+    } finally {
+      NProgress.done()
+    }
+  },
+  beforeRouteUpdate() {
+    NProgress.start()
+    EventService.getEvents(2, this.page)
+      .then(response => {
+        this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
+      })
+      .catch(() => {
+        return { name: 'NetworkError' }
+      })
+      .finally(() => {
+        NProgress.done()
+      })
   },
   computed: {
     hasNextPage() {
